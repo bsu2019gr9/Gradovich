9bf334cfdb4645337ceb57b6a14f42f6e3eacc13
Leeth-gradovich <59262526+Leeth-gradovich@users.noreply.github.com>
2020-02-20 09:48:13 +0300

    Add files via upload

---
+++ functionsArray/functionsArrayGradovich.h
@@ -0,0 +1,23 @@
+void inputArray(int*, const int); //ââîä ìàññèâà ñ êëàâèàòóðû
+void printArray(int*, const int);//âûâîä ìàññèâà
+void initArray(int*, const int, int, int);//èíèöèàëèçàöèÿ ìàññèâà
+int findMax(int*, const int);//ìàêñèìóì
+int findMin(int*, const int);//ìèíèìóì
+int sumArray(int*, const int);//ñóììà
+void reverse(int*, int*, const int);//ïåðåâåðíóòûé
+void halfArray(int*, int&);//ïîëîâèíà ìàññèâà
+void divArray(int*, const int);//ðàçäåëåíèå ìàññèâà íà äâà
+void shiftArrayRight(int*, const int);//ñäâèã âïðàâî
+void shiftArrayLeft(int*, const int);//ñäâèã âëåâî
+void bubbleSort(int*, const int);//ïóçûðüêîâàÿ ñîðòèðîâêà
+void shakerSort(int*, const int);//øåéêåðíàÿ ñîðòèðîâêà
+void stupidSort(int*, const int);//ãëóïàÿ ñîðòèðîâêà
+void oddEvenSort(int*, const int);//÷åòíî-íå÷åòíàÿ ñîðòèðîâêà
+int getNextGap(int);//ðàññòîÿíèå
+void combSort(int*, const int);//ñîðòèðîâêà ðàñ÷åñêîé
+void ShellSort(int*, const int);//ñîðòèðîâêà Øåëëà
+void selectionSort(int*, const int);//ñîðòèðîâêà âûáîðîì
+void menuArr();//ìåíþ
+int* giveMemory(int);//âûäåëåíèå ïàìÿòè
+void freeMemory(int*);//îñâîáîäèòü ïàìÿòü
+
