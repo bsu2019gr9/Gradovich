2957faffd674802d0d262d16518555f9a779291b
Leeth-gradovich <59262526+Leeth-gradovich@users.noreply.github.com>
2020-02-26 22:50:44 +0300

    Add files via upload

---
+++ functionsArray/functionsArrayGradovich.h
@@ -0,0 +1,17 @@
+void mkArrOfInt(int, int*&);//создать массив
+void inputArray(int*, int*); //ввод массива с клавиатуры
+void initRandomArray(int*, int*, const int, const int);//инициализация псевдослучайными числами
+void initArray(int*, const int, int, int);//инициализация массива
+void printArray(int*, int*);//вывод массива
+int findMax(int*, int*);//максимум
+int findMin(int*, int*);//минимум
+int sumArray(int*, int*);//сумма
+void reverse(int*, int*);//перевернутый
+void divArray(int*, int*, int, int);//разделение массива на два
+void shiftArrayRight(int*, int*);//сдвиг вправо на 1 
+void shiftArrayLeft(int*, int*);//сдвиг влево на 1
+void bubbleSort(int*, int*);//пузырьковая сортировка
+void stupidSort(int*, int*);//глупая сортировка
+void freeMemory(int*);//освободить память
+void countRepeatingElements(int*, int*);//количество повторяющихся элементов
+void countRepeatingInARow(int*, int*);//количество повторяющихся подряд элементов
