4596ae9dbc8680669cb06748309c9c8b14e13f7f
Leeth-gradovich <59262526+Leeth-gradovich@users.noreply.github.com>
2020-06-05 22:27:10 +0300

    Update student.h

--- final work/student.h
+++ final work/student.h
@@ -18,69 +18,69 @@ private:
 	int satisfaction;
 public:
 
-	Student();//конструктор без параметров
-	//конструктор с параметрами
+	Student();//РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ Р±РµР· РїР°СЂР°РјРµС‚СЂРѕРІ
+	//РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ СЃ РїР°СЂР°РјРµС‚СЂР°РјРё
 	Student(const char[30], const char[30], const char[30], const int, const int, const int[10], const float, const float, const int, const int);
-	//~Student();//декструктор
+	//~Student();//РґРµРєСЃС‚СЂСѓРєС‚РѕСЂ
 
-	Student(const Student&);//конструктор копирования
-	const Student& operator=(const Student&);//оператор присваивания
+	Student(const Student&);//РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РєРѕРїРёСЂРѕРІР°РЅРёСЏ
+	const Student& operator=(const Student&);//РѕРїРµСЂР°С‚РѕСЂ РїСЂРёСЃРІР°РёРІР°РЅРёСЏ
 
 	friend ostream& operator<<(ostream&, const Student&);
 	friend istream& operator>>(istream&, Student&);
 
-	void setSurname(const char*);//задать фамилию
-	void setName(const char*);//задать имя
-	void setPatronymic(const char*);//задать отчество
-	void setCourse(int);//задать курс
-	void setGroup(int);//задать группу
-	void setMarks(int*);//задать оценки
-	void setScholarship(float);//задать стипендию
-	void setStudent(const char*, const char*, const char*, int, int, int*, float);//задать студента
-	void setStudent();//задать студента без параметров
+	void setSurname(const char*);//Р·Р°РґР°С‚СЊ С„Р°РјРёР»РёСЋ
+	void setName(const char*);//Р·Р°РґР°С‚СЊ РёРјСЏ
+	void setPatronymic(const char*);//Р·Р°РґР°С‚СЊ РѕС‚С‡РµСЃС‚РІРѕ
+	void setCourse(int);//Р·Р°РґР°С‚СЊ РєСѓСЂСЃ
+	void setGroup(int);//Р·Р°РґР°С‚СЊ РіСЂСѓРїРїСѓ
+	void setMarks(int*);//Р·Р°РґР°С‚СЊ РѕС†РµРЅРєРё
+	void setScholarship(float);//Р·Р°РґР°С‚СЊ СЃС‚РёРїРµРЅРґРёСЋ
+	void setStudent(const char*, const char*, const char*, int, int, int*, float);//Р·Р°РґР°С‚СЊ СЃС‚СѓРґРµРЅС‚Р°
+	void setStudent();//Р·Р°РґР°С‚СЊ СЃС‚СѓРґРµРЅС‚Р° Р±РµР· РїР°СЂР°РјРµС‚СЂРѕРІ
 
-	void editSurname(const char*);//редактировать фамилию
-	void editName(const char*);//редактировать имя
-	void editPatronymic(const char*);//редактировать отчество
-	void editCourse(int);//редактировать курс
-	void editGroup(int);//редактировать группу
-	void editMarks(int*);//редактировать оценки
-	void editScholarship(float);//редактировать стипендию
-	void editStudent(const char*, const char*, const char*, int, int, int*, float);//редактировать студента
-	void editStudent();//редактировать студента без параметров
+	void editSurname(const char*);//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ С„Р°РјРёР»РёСЋ
+	void editName(const char*);//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ РёРјСЏ
+	void editPatronymic(const char*);//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ РѕС‚С‡РµСЃС‚РІРѕ
+	void editCourse(int);//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ РєСѓСЂСЃ
+	void editGroup(int);//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ РіСЂСѓРїРїСѓ
+	void editMarks(int*);//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ РѕС†РµРЅРєРё
+	void editScholarship(float);//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ СЃС‚РёРїРµРЅРґРёСЋ
+	void editStudent(const char*, const char*, const char*, int, int, int*, float);//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ СЃС‚СѓРґРµРЅС‚Р°
+	void editStudent();//СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ СЃС‚СѓРґРµРЅС‚Р° Р±РµР· РїР°СЂР°РјРµС‚СЂРѕРІ
 
-	void deleteSurname();//удалить фамилию
-	void deleteName();//удалить имя
-	void deletePatronymic();//удалить отчество
-	void deleteCourse();//удалить курс
-	void deleteGroup();//удалить группу
-	void deleteMarks();//удалить оценки
-	void deleteScholarship();//удалить стипендию
-	void deleteStudent();//удалить студента
+	void deleteSurname();//СѓРґР°Р»РёС‚СЊ С„Р°РјРёР»РёСЋ
+	void deleteName();//СѓРґР°Р»РёС‚СЊ РёРјСЏ
+	void deletePatronymic();//СѓРґР°Р»РёС‚СЊ РѕС‚С‡РµСЃС‚РІРѕ
+	void deleteCourse();//СѓРґР°Р»РёС‚СЊ РєСѓСЂСЃ
+	void deleteGroup();//СѓРґР°Р»РёС‚СЊ РіСЂСѓРїРїСѓ
+	void deleteMarks();//СѓРґР°Р»РёС‚СЊ РѕС†РµРЅРєРё
+	void deleteScholarship();//СѓРґР°Р»РёС‚СЊ СЃС‚РёРїРµРЅРґРёСЋ
+	void deleteStudent();//СѓРґР°Р»РёС‚СЊ СЃС‚СѓРґРµРЅС‚Р°
 
-	void getSurname();//вывести фамилию
-	void getName();//вывести имя
-	void getPatronymic();//вывести отчество
-	void getCourse();//вывести курс
-	void getGroup();//вывести группу
-	void getMarks();//вывести оценки
-	void getAverageMark();//вывести среднюю оценку
-	void getScholarship();//вывести стипендию
-	void getStudent();//вывести информацию о студенте
+	void getSurname();//РІС‹РІРµСЃС‚Рё С„Р°РјРёР»РёСЋ
+	void getName();//РІС‹РІРµСЃС‚Рё РёРјСЏ
+	void getPatronymic();//РІС‹РІРµСЃС‚Рё РѕС‚С‡РµСЃС‚РІРѕ
+	void getCourse();//РІС‹РІРµСЃС‚Рё РєСѓСЂСЃ
+	void getGroup();//РІС‹РІРµСЃС‚Рё РіСЂСѓРїРїСѓ
+	void getMarks();//РІС‹РІРµСЃС‚Рё РѕС†РµРЅРєРё
+	void getAverageMark();//РІС‹РІРµСЃС‚Рё СЃСЂРµРґРЅСЋСЋ РѕС†РµРЅРєСѓ
+	void getScholarship();//РІС‹РІРµСЃС‚Рё СЃС‚РёРїРµРЅРґРёСЋ
+	void getStudent();//РІС‹РІРµСЃС‚Рё РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ СЃС‚СѓРґРµРЅС‚Рµ
 
-	void countAverageMark(int*);//посчитать среднюю оценку
+	void countAverageMark(int*);//РїРѕСЃС‡РёС‚Р°С‚СЊ СЃСЂРµРґРЅСЋСЋ РѕС†РµРЅРєСѓ
 
-	void determineStudentStatus();//определить статус студента
-	void showStudentWithGoodStatus();//показать студента с "хорошими" оценками
-	void showStudentWithWellStatus();//показать студента с "отличными" оценками
-	void showStudentWithSatisfactoryMarks();//показать студента с "удовлетворительными" оценками
-	void showStudentWithUnsatisfactoryMarks();//показать студента с "неудовлетворительными" оценками
+	void determineStudentStatus();//РѕРїСЂРµРґРµР»РёС‚СЊ СЃС‚Р°С‚СѓСЃ СЃС‚СѓРґРµРЅС‚Р°
+	void showStudentWithGoodStatus();//РїРѕРєР°Р·Р°С‚СЊ СЃС‚СѓРґРµРЅС‚Р° СЃ "С…РѕСЂРѕС€РёРјРё" РѕС†РµРЅРєР°РјРё
+	void showStudentWithWellStatus();//РїРѕРєР°Р·Р°С‚СЊ СЃС‚СѓРґРµРЅС‚Р° СЃ "РѕС‚Р»РёС‡РЅС‹РјРё" РѕС†РµРЅРєР°РјРё
+	void showStudentWithSatisfactoryMarks();//РїРѕРєР°Р·Р°С‚СЊ СЃС‚СѓРґРµРЅС‚Р° СЃ "СѓРґРѕРІР»РµС‚РІРѕСЂРёС‚РµР»СЊРЅС‹РјРё" РѕС†РµРЅРєР°РјРё
+	void showStudentWithUnsatisfactoryMarks();//РїРѕРєР°Р·Р°С‚СЊ СЃС‚СѓРґРµРЅС‚Р° СЃ "РЅРµСѓРґРѕРІР»РµС‚РІРѕСЂРёС‚РµР»СЊРЅС‹РјРё" РѕС†РµРЅРєР°РјРё
 
-	int getIntGroup() const;//геттер номера группы
-	int getIntCourse() const;//геттер номера курса
-	int getIntSatisfaction() const;//геттер статуса удовлетворительности оценок
-	int getIntStatus();//геттер статуса оценок для стипендии
-	float getFloatAverageMark();//геттер средней оценки
-	float getFloatScholarship() const;//геттер стипендии
-	char* getCharSurname() const;//геттер фамилии
+	int getIntGroup() const;//РіРµС‚С‚РµСЂ РЅРѕРјРµСЂР° РіСЂСѓРїРїС‹
+	int getIntCourse() const;//РіРµС‚С‚РµСЂ РЅРѕРјРµСЂР° РєСѓСЂСЃР°
+	int getIntSatisfaction() const;//РіРµС‚С‚РµСЂ СЃС‚Р°С‚СѓСЃР° СѓРґРѕРІР»РµС‚РІРѕСЂРёС‚РµР»СЊРЅРѕСЃС‚Рё РѕС†РµРЅРѕРє
+	int getIntStatus();//РіРµС‚С‚РµСЂ СЃС‚Р°С‚СѓСЃР° РѕС†РµРЅРѕРє РґР»СЏ СЃС‚РёРїРµРЅРґРёРё
+	float getFloatAverageMark();//РіРµС‚С‚РµСЂ СЃСЂРµРґРЅРµР№ РѕС†РµРЅРєРё
+	float getFloatScholarship() const;//РіРµС‚С‚РµСЂ СЃС‚РёРїРµРЅРґРёРё
+	char* getCharSurname() const;//РіРµС‚С‚РµСЂ С„Р°РјРёР»РёРё
 };
